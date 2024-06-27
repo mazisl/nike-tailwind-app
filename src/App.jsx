bdc67163991273ba5165d53803eb57f00e2bae69
Maz <maz.islam1@gmail.com>
2024-06-27 10:10:41 +0400

    hero section started

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import {Hero, PopularProducts, SuperQuality, Services, SpecialOffer, CustomerReviews, Subscribe, Footer} from './sections';
+import Nav from './components/Nav';
 
 const App = () => {
   return (
     <main className="relative">
-      {/* <Nav /> */}
+      <Nav />
       <section className="xl:padding-1 wide:padding-r padding-b">
         <Hero />
       </section>
