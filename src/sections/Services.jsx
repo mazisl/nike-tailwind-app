3ed3ba0573aa94b399f1ce6acc4f357b1959cfd5
Maz <maz.islam1@gmail.com>
2024-06-28 10:29:39 +0400

    initial development complete

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,6 +1,13 @@
+import { services } from "../constants";
+import ServiceCard from "../components/ServiceCard";
+
 const Services = () => {
   return (
-    <div>Services</div>
+    <section className="max-container flex flex-wrap justify-center gap-9">
+      {services.map((service) => (
+        <ServiceCard key={service.label} {...service} />
+      ))}
+    </section>
   )
 }
 
